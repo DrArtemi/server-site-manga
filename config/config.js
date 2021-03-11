1f047263ef37ca640ae9c8a2e904bc625af81462
DrArtemi <adrien.milcent@outlook.com>
2021-03-11 11:16:31 +0100

    init server apollo-server + sqlite3 + graphql

---
+++ config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "development": {
+        "dialect": "sqlite",
+        "storage": "./database.sqlite3"
+    },
+    "test": {
+        "dialect": "sqlite",
+        "storage": ":memory"
+    },
+    "production": {
+        "dialect": "sqlite",
+        "storage": "./database.sqlite3"
+    }
+}
---
+++ server.js
@@ -0,0 +1,32 @@
+const { ApolloServer } = require('apollo-server');
+const jwt = require('jsonwebtoken');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers/resolver');
+require('dotenv').config()
+
+
+const getUser = token => {
+  try {
+    if (token) {
+      return jwt.verify(token, process.env.JWT_SECRET)
+    }
+    return null
+  } catch (error) {
+    return null
+  }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => {
+        const token = req.get('Authorization') || ''
+        return { user: getUser(token.replace('Bearer', ''))}
+    },
+    introspection: true,
+    playground: true
+});
+
+server.listen({ port: process.env.PORT || 8000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
